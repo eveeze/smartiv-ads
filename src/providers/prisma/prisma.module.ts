13aa0ba874eb746c6af60759d6939d82067acce6
eveeze <jsah737@gmail.com>
2025-12-27 22:32:07 +0700

    feat: update

--- src/providers/prisma/prisma.module.ts
+++ src/providers/prisma/prisma.module.ts
@@ -3,6 +3,6 @@ import { PrismaService } from './prisma.service';
 
 @Module({
   providers: [PrismaService],
-  exports: [PrismaService], // <--- WAJIB DITAMBAHKAN
+  exports: [PrismaService],
 })
 export class PrismaModule {}
